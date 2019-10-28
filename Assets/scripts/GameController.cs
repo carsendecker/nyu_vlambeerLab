dcce68590f42cda0c0433f5b3d6bfe9d913b4214
Carsen Decker <cad631@nyu.edu>
2019-10-28 13:30:06 -0400

    proc gen lab, done with main part

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Pathmaker.globalTileCount = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
--- Assets/scripts/Pathmaker.cs
+++ Assets/scripts/Pathmaker.cs
@@ -9,79 +9,67 @@ using UnityEngine;
 // STEP 1: ======================================================================================
 // put this script on a Sphere... it will move around, and drop a path of floor tiles behind it
 
-public class Pathmaker : MonoBehaviour {
+public class Pathmaker : MonoBehaviour
+{
+	public static int globalTileCount;
+	public Transform pathmakerSpherePrefab;
+	public Transform[] TileList;
 
-// STEP 2: ============================================================================================
-// translate the pseudocode below
+	[Space(5)] [Header("Generation Values")]
+	public int maxTilesToSpawn = 50;
+	public int minTilesToSpawn;
+	[Range(0, 1)] public float turnRightChance;
+	[Range(0, 1)] public float turnLeftChance;
+	[Range(0, 1)] public float minSpawnChance;
+	[Range(0, 1)] public float maxSpawnChance;
 
-//	DECLARE CLASS MEMBER VARIABLES:
-//	Declare a private integer called counter that starts at 0; 		// counter var will track how many floor tiles I've instantiated
-//	Declare a public Transform called floorPrefab, assign the prefab in inspector;
-//	Declare a public Transform called pathmakerSpherePrefab, assign the prefab in inspector; 		// you'll have to make a "pathmakerSphere" prefab later
+	private int tilesToSpawn;
+	private float spawnChance;
+	private int counter;
 
-
-	void Update () {
-//		If counter is less than 50, then:
-//			Generate a random number from 0.0f to 1.0f;
-//			If random number is less than 0.25f, then rotate myself 90 degrees;
-//				... Else if number is 0.25f-0.5f, then rotate myself -90 degrees;
-//				... Else if number is 0.99f-1.0f, then instantiate a pathmakerSpherePrefab clone at my current position;
-//			// end elseIf
-
-//			Instantiate a floorPrefab clone at current position;
-//			Move forward ("forward", as in, the direction I'm currently facing) by 5 units;
-//			Increment counter;
-//		Else:
-//			Destroy my game object; 		// self destruct if I've made enough tiles already
+	private void Start()
+	{
+		tilesToSpawn = Random.Range(minTilesToSpawn, maxTilesToSpawn);
+		spawnChance = Random.Range(minSpawnChance, maxSpawnChance);
 	}
 
-} // end of class scope
+	void Update () {
 
-// MORE STEPS BELOW!!!........
+		if (counter < tilesToSpawn || globalTileCount < 500)
+		{
+			float rand = Random.value;
+			
+			if (rand <= turnRightChance)
+				transform.Rotate(0, 90, 0);
+			else if(rand <= turnRightChance + turnLeftChance)
+				transform.Rotate(0, -90, 0); 
+			else if (rand >= spawnChance && rand <= 1f)
+				Instantiate(pathmakerSpherePrefab, transform.position, Quaternion.identity);
 
+			//Make a random chance for something besides a normal road tile to spawn
+			float specialTileChance = Random.Range(0, 10);
+			if (specialTileChance < 2)
+			{
+				Instantiate(TileList[Random.Range(1, TileList.Length)], transform.position, transform.rotation);
+			}
+			else
+			{
+				Instantiate(TileList[0], transform.position, Quaternion.identity);
+			}
 
+			transform.Translate(transform.forward * 5, Space.World);
 
+			counter++;
+			globalTileCount++;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+	
 
-// STEP 3: =====================================================================================
-// implement, test, and stabilize the system
-
-//	IMPLEMENT AND TEST:
-//	- save your scene!!! the code could potentially be infinite / exponential, and crash Unity
-//	- put Pathmaker.cs on a sphere, configure all the prefabs in the Inspector, and test it to make sure it works
-//	STABILIZE: 
-//	- code it so that all the Pathmakers can only spawn a grand total of 500 tiles in the entire world; how would you do that?
-//	- (hint: declare a "public static int" and have each Pathmaker check this "globalTileCount", somewhere in your code? if there are already enough tiles, then maybe the Pathmaker could Destroy my game object
-
-
-
-// STEP 4: ======================================================================================
-// tune your values...
-
-// a. how long should a pathmaker live? etc.
-// b. how would you tune the probabilities to generate lots of long hallways? does it work?
-// c. tweak all the probabilities that you want... what % chance is there for a pathmaker to make a pathmaker? is that too high or too low?
-
-
-
-// STEP 5: ===================================================================================
-// maybe randomize it even more?
-
-// - randomize 2 more variables in Pathmaker.cs for each different Pathmaker... you would do this in Start()
-// - maybe randomize each pathmaker's lifetime? maybe randomize the probability it will turn right? etc. if there's any number in your code, you can randomize it if you move it into a variable
-
-
-
-// STEP 6:  =====================================================================================
-// art pass, usability pass
-
-// - move the game camera to a position high in the world, and then point it down, so we can see your world get generated
-// - CHANGE THE DEFAULT UNITY COLORS, PLEASE, I'M BEGGING YOU
-// - add more detail to your original floorTile placeholder -- and let it randomly pick one of 3 different floorTile models, etc. so for example, it could randomly pick a "normal" floor tile, or a cactus, or a rock, or a skull
-//		- MODEL 3 DIFFERENT TILES IN MAYA! DON'T STOP USING MAYA OR YOU'LL FORGET IT ALL
-//		- add a simple in-game restart button; let us press [R] to reload the scene and see a new level generation
-// - with Text UI, name your proc generation system ("AwesomeGen", "RobertGen", etc.) and display Text UI that tells us we can press [R]
-
-
+}
 
 // OPTIONAL EXTRA TASKS TO DO, IF YOU WANT: ===================================================
 
